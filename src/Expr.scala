97ef186ef705c31b2c17553806a3899166100c62
reinaldo <rqmdrno23@gmail.com>
2026-01-11 13:15:35 -0400

    Extend arithmetic expression to support identifiers

--- src/Expr.scala
+++ src/Expr.scala
@@ -2,4 +2,6 @@ trait Expr
 case class Num(num: Int) extends Expr
 case class Add(left: Expr, right: Expr) extends Expr
 case class Sub(left: Expr, right: Expr) extends Expr
+case class Val(x: String, i: Expr, b: Expr) extends Expr
+case class Id(x: String) extends Expr
 
