d67b8e9405344fb96c8fb9f1d580d8f710c54dd9
reinaldo <rqmdrno23@gmail.com>
2026-01-13 15:02:33 -0400

    Extend eval to handle identifiers

--- src/Eval.scala
+++ src/Eval.scala
@@ -1,6 +1,10 @@
-def eval(ex: Expr): Int = 
+def eval(ex: Expr, env: Env): Int = 
     ex match 
         case Num(num) => num
-        case Add(left, right) => eval(left) + eval(right)
-        case Sub(left, right) => eval(left) - eval(right)
-    
+        case Add(left, right) => eval(left, env) + eval(right, env)
+        case Sub(left, right) => eval(left, env) - eval(right, env)
+        case Val(x, i, b) => eval(b, env + (x -> eval(i, env)))
+        case Id(x) => lookup(x, env)
+
+def lookup(id: String, env: Env): Int = 
+    env.getOrElse(id, throw new RuntimeException(s"free variable: $id"))
