9327759eee94c3429981201c08843534e83172ee
reinaldo <rqmdrno23@gmail.com>
2026-01-16 10:57:45 -0400

    Move Expr into ast package

--- src/ast/Expr.scala
+++ src/ast/Expr.scala
@@ -1,7 +1,11 @@
+package ast
+
 trait Expr
 case class Num(num: Int) extends Expr
 case class Add(left: Expr, right: Expr) extends Expr
 case class Sub(left: Expr, right: Expr) extends Expr
 case class Val(x: String, i: Expr, b: Expr) extends Expr
 case class Id(x: String) extends Expr
+case class App(f: String, a: Expr) extends Expr
 
+case class Fdef(f: String, x: String, b: Expr) 
